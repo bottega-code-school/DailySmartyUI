06c7267501071a82454219eb4b33939f30da3df3
Max <maxnelson997@gmail.com>
2018-04-28 17:06:58 -0600

    styled the recent-posts post component

--- src/components/post.js
+++ src/components/post.js
@@ -37,7 +37,7 @@ class Post extends Component {
             return (
                 <li className="recent-post">
                     <div className="recent-post__title">
-                        {this.props.title}
+                        <a href={this.props.url_for_post}>{this.props.title}</a>
                     </div>
                     <div className="recent-post__topics">
                         {this.renderTopics()}
